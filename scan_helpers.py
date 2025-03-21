d6308bef678678541330ab81fb28ef62f9b8ad0f
Ben Eck <eck.mben@gmail.com>
2025-03-21 10:14:59 -0600

    added function to import all fingerprints in a directory, may or may not work. also changed import_scan_fp to set index to Date/Time

--- scan_helpers.py
+++ scan_helpers.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from scipy.signal import savgol_filter
 import matplotlib.pyplot as plt
+import os
 
 def plot_scan_fp(df, measurement, **kwargs):
     df.T.drop('Date/Time').plot(y=measurement, xlabel = 'Wavelength (nm)', ylabel= 'Absorbance', **kwargs)
@@ -8,8 +9,18 @@ def plot_scan_fp(df, measurement, **kwargs):
 def import_scan_fp(path):
     df = pd.read_csv(path, sep='\t', header=1).drop('Status_0', axis=1)
     df['Date/Time'] = pd.to_datetime(df['Date/Time'])
-    #df.set_index('Date/Time', inplace=True)
+    df.set_index('Date/Time', inplace=True)
     return df
+
+def get_fp_files(directory):
+    # List all files in the directory ending with .fp
+    return [f for f in os.listdir(directory) if f.endswith('.fp')]
+
+
+def import_all_scan_fp(directory):
+    paths = get_fp_files(directory)
+    files = [import_scan_fp(path) for path in paths]
+    return pd.concat(files).drop_duplicates().reset_index()
 
 def apply_calibrations(df, calibrations, calibrations_kwargs):
     for calibration in calibrations:
