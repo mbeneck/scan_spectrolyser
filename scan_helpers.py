8f2c51f0e65001bc84d6acec7a9155821762db3d
Ben Eck <eck.mben@gmail.com>
2025-07-07 15:46:32 -0600

    added import_all_scan_fp function that can check for daylight savings time and make corrections

--- scan_helpers.py
+++ scan_helpers.py
@@ -3,26 +3,48 @@ from scipy.signal import savgol_filter
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from datetime import datetime, timedelta
+import pytz
 
-def plot_scan_fp(df, measurement, **kwargs):
-    df.T.plot(y=measurement, xlabel = 'Wavelength (nm)', ylabel= 'Absorbance', **kwargs)
+def get_fp_files(directory):
+    # List all files in the directory ending with .fp
+    return [directory + f for f in os.listdir(directory) if f.endswith('.fp')]
+
+def is_dst(timezone_name, dt):
+    """
+    Checks if a given datetime is within DST for a specified timezone.
+
+    Args:
+        timezone_name: The name of the timezone (e.g., 'US/Eastern').
+        dt: The datetime object to check.
+
+    Returns:
+        True if the datetime is within DST, False otherwise.
+    """
+    timezone = pytz.timezone(timezone_name)
+    localized_dt = timezone.localize(dt)
+    return localized_dt.dst() != timedelta(0)
 
 def import_scan_fp(path):
     df = pd.read_csv(path, sep='\t', header=1).drop('Status_0', axis=1)
     df['Date/Time'] = pd.to_datetime(df['Date/Time'])
     df.set_index('Date/Time', inplace=True)
     df.columns = df.columns.astype(float)
-    return df
+    return df  
 
-def get_fp_files(directory):
-    # List all files in the directory ending with .fp
-    return [directory + f for f in os.listdir(directory) if f.endswith('.fp')]
-
-
-def import_all_scan_fp(directory):
+def import_all_scan_fp(directory, correct_dst=False, tz='UTC'):
     paths = get_fp_files(directory)
     files = [import_scan_fp(path) for path in paths]
+
+    for file in files:
+        if correct_dst and is_dst(tz,file.index[0]):
+            file.index = file.index - pd.Timedelta('1h')
+
     return pd.concat(files).drop_duplicates().sort_values(by='Date/Time')
+
+
+def plot_scan_fp(df, measurement, **kwargs):
+    df.T.plot(y=measurement, xlabel = 'Wavelength (nm)', ylabel= 'Absorbance', **kwargs)
 
 def apply_calibrations(df, calibrations, calibrations_kwargs):
     for calibration in calibrations:
